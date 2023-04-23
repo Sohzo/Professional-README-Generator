d9a44542bbafc55c78f266fbef1ddc9882f02373
Jonathan Winter <jonpw2004@outlook.com>
2023-04-23 14:50:46 -0600

    Created README structure

---
+++ README.md
@@ -0,0 +1,41 @@
+# Node Project
+  ![MIT](https://img.shields.io/badge/license-MIT-blue)
+
+  ## Description
+
+  test
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Test Instructions](#test-instructions)
+  * [Questions](#questions)
+
+  ## Installation
+
+  test
+
+  ## Usage
+
+  test
+
+  ## License
+
+  Project licensed under MIT
+
+  ## Contributing
+
+  test
+
+  ## Test Instructions
+
+  test
+
+  ## Questions
+
+  * [Github](https://github.com/test)
+  * Please email me with any questions at test
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -25,11 +25,57 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "") {
+    return "";
+  } else {
+    return renderLicenseBadge(license);
+  };
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseSection(data.license)}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Test Instructions](#test-instructions)
+  * [Questions](#questions)
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usageinfo}
+
+  ## License
+
+  Project licensed under ${data.license}
+
+  ## Contributing
+
+  ${data.contribution}
+
+  ## Test Instructions
+
+  ${data.testinstructions}
+
+  ## Questions
+
+  * [Github](https://github.com/${data.github})
+  * Please email me with any questions at ${data.email}
+
 `;
 }
 
